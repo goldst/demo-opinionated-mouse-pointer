9a5c65e07a5c6fe60f36c439f68a4e7e78471e91
Leonard Goldstein <leonard-goldstein@outlook.de>
2022-12-26 23:37:14 +0100

    Minor bug fixes, styling, documentation

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import { HatchButton, HatchFixedWidth, HatchInputField, HatchRadio, HatchToggle, HatchWrapper } from '@hatch/hds'
-import '@hatch/hds/dist/global.min.css'
-import '@hatch/hds/dist/T1.min.css'
+import { BaseSyntheticEvent, SyntheticEvent, useEffect, useState } from 'react';
+import './App.css';
+import { HatchButton, HatchFixedWidth, HatchRadio, HatchToggle, HatchWrapper } from '@hatch/hds';
+import '@hatch/hds/dist/global.min.css';
+import '@hatch/hds/dist/T1.min.css';
 
 function App() {
-    const [[x, y], setXY] = useState<[number, number]>([0, 0]);
+    const [[x, y], setXY] = useState<[number, number]>([200, 200]);
     const [locked, setLocked] = useState(false);
+    const [showLeave, setShowLeave] = useState(false);
+    const [pointerLockListener, setPointerLockListener] = useState<() => void>();
+    const [mousemoveListener, setMousemoveListener] = useState<(e: MouseEvent) => void>();
+    const [intensity, setIntensity] = useState<number>(50);
+
+    useEffect(() => {
+        const clickListener = () => {
+            if(locked) {
+                const element = document.elementFromPoint(x, y);
+
+                (document.elementFromPoint(x, y) as any)?.click();
+                (document.elementFromPoint(x, y) as any)?.focus();
+            }
+        };
+
+        document.addEventListener('click', clickListener);
+
+        return () => document.removeEventListener('click', clickListener);
+    }, [locked, x, y, locked]);
 
     const updatePosition = (e: MouseEvent, gravityPoints: ([number, number])[], rects: DOMRect[]) => {
         setXY(([x, y]) => {
-            /*if(rects.some(rect =>
-                rect.left < x &&
-                rect.left + rect.width > x &&
-                rect.top < y &&
-                rect.top + rect.height > y
-            )) {
-                return [
-                    x + e.movementX*0.8,
-                    y + e.movementY*0.8,
-                ];
-            }*/
-            
-            //console.log('gravityPoints', gravityPoints);
-
-            //console.log('e', e);
-
             const [middleX, middleY] = [
                 x + (e.movementX/2),
                 y + (e.movementY/2),
             ];
-            
-            //console.log('middle', middleX, middleY);
 
             const length = Math.sqrt(e.movementX**2 + e.movementY**2);
-
-            //console.log('length', length);
 
             const [influenceX, influenceY] = gravityPoints.map(([gx, gy]) => {
                 const [absoluteX, absoluteY] = [
@@ -45,8 +43,6 @@ function App() {
                 ];
 
                 const gravityPointLength = Math.sqrt(absoluteX**2 + absoluteY**2);
-
-                console.log(gravityPointLength);
 
                 if(gravityPointLength > 100) {
                     return [0, 0];
@@ -59,49 +55,54 @@ function App() {
             })
                 .reduce(([ax, ay], [bx, by]) => [ax+bx, ay+by], [0, 0]);
 
-            //console.log('influence', influenceX, influenceY);
-
             const [weightedInfluenceX, weightedInfluenceY] = [
-                (influenceX/gravityPoints.length)*length*6 || 0,
-                (influenceY/gravityPoints.length)*length*6 || 0
+                (influenceX/gravityPoints.length)*length*6*(intensity/50) || 0,
+                (influenceY/gravityPoints.length)*length*6*(intensity/50) || 0
             ];
 
-            //console.log('weightedInfluence', weightedInfluenceX, weightedInfluenceY);
-
-            console.log('----------------');
             const screen = document.body.getBoundingClientRect();
-            return [
+
+            const pos: [number, number] = [
                 Math.max(8, Math.min(x + (e.movementX + weightedInfluenceX)/2, screen.width-8)),
                 Math.max(8, Math.min(y + (e.movementY + weightedInfluenceY)/2, screen.height-8))
             ];
+
+            setShowLeave(pos[0] < 48 || pos[1] < 48 || pos[0] > screen.width - 48 || pos[1] > screen.height - 48);
+
+            return pos;
         });
     }
 
     const lockChangeAlert = (gravityPoints: ([number, number])[], domRects: DOMRect[]) => {
-        const listener = (e: MouseEvent) => updatePosition(e, gravityPoints, domRects);
+        setLocked(false);
 
         if (document.pointerLockElement === document.body ||
             (document as any).mozPointerLockElement === document.body) {
-            console.log('The pointer lock status is now locked');
-            document.addEventListener("mousemove", listener, false);
+
+            const newListener = (e: MouseEvent) => updatePosition(e, gravityPoints, domRects);
+            setMousemoveListener(() => newListener);
+            
+            setLocked(true);
+            document.addEventListener('mousemove', newListener, false);
         } else {
-            console.log('The pointer lock status is now unlocked');
             setLocked(false);
-            document.removeEventListener("mousemove", listener, false);
+            if(pointerLockListener) document.removeEventListener('pointerlockchange', pointerLockListener, false);
+            if(mousemoveListener) document.removeEventListener('mousemove', mousemoveListener, false);
         }
     }
 
-    const changeToggle = (to: boolean) => {
-        setLocked(to);
-        console.log(to);
-        if (!to) {
+    const changeToggle = (to: BaseSyntheticEvent) => {
+        setLocked(to.target.checked);
+        if (!to.target.checked) {
+            if(pointerLockListener) document.removeEventListener('pointerlockchange', pointerLockListener, false);
+            if(mousemoveListener) document.removeEventListener('mousemove', mousemoveListener, false);
+            document.exitPointerLock();
             return;
         }
 
-
         const newGravityPoints: ([number, number])[] = [];
 
-        const rects = Array.from(document.querySelectorAll('.hatch-button--primary, .bp4-control-indicator'))
+        const rects = Array.from(document.querySelectorAll('.hatch-button--primary, .hatch-radio__label:not(.hatch-radio__disabled) .bp4-control-indicator, .hatch-toggle-slider'))
             .map(element => element.getBoundingClientRect());
 
         rects.forEach(rect => {
@@ -111,96 +112,131 @@ function App() {
             ]);
         });
 
-        console.log('new GravityPoints', newGravityPoints);
-
         document.body.requestPointerLock = document.body.requestPointerLock ||
             (document.body as any).mozRequestPointerLock;
 
         document.body.requestPointerLock();
 
-        if ("onpointerlockchange" in document) {
-            document.addEventListener('pointerlockchange', () => lockChangeAlert(newGravityPoints, rects), false);
-        } else if ("onmozpointerlockchange" in document) {
-            (document as any).addEventListener('mozpointerlockchange', () => lockChangeAlert(newGravityPoints, rects), false);
+        if(pointerLockListener) {
+            document.removeEventListener('pointerlockchange', pointerLockListener, false);
         }
 
+        const newPointerLockListener = () => lockChangeAlert(newGravityPoints, rects);
+        setPointerLockListener(() => newPointerLockListener);
+
+        if ('onpointerlockchange' in document) {
+            document.addEventListener('pointerlockchange', newPointerLockListener, false);
+        } else if ('onmozpointerlockchange' in document) {
+            (document as any).addEventListener('mozpointerlockchange', newPointerLockListener, false);
+        }  
     }
 
     return (
-        <HatchFixedWidth maxInnerWidth='1200px' minPaddingLR='16px' pos='center'>
-            <h1>Demo: Opinionated Mouse Pointer</h1>
-            <HatchWrapper direction='row' space='--space-3'>
-                <HatchWrapper>
+        <HatchFixedWidth maxInnerWidth='1100px' minPaddingLR='16px' pos='center' style={{ overflowY: 'auto' }} space='--space-3'>
+            <HatchWrapper direction='row' space='--space-2'>
+                <a className="logo" href="https://goldst.dev">
+                    <div className="logo-image"></div>
+                </a>
+                <h1>
+                    Experiment: Opinionated Mouse Pointer
+                </h1>
+            </HatchWrapper>
+            
+            <HatchWrapper direction='row' space='--space-6' style={{ flexWrap: 'wrap' }}>
+                <HatchWrapper style={{ minWidth: '200px', flexGrow: 1, width: 0 }}>
                     <h3>Options</h3>
-                    <HatchToggle checked={locked} label='Enable' onChange={to => changeToggle(to)} />
+                    <HatchToggle checked={locked} label='Enable' onChange={(to: SyntheticEvent) => changeToggle(to)} />
+                    <label>
+                        <div>Gravity: {intensity}%</div>
+                        <input className='range' disabled={locked} type="range" min="1" max="100" value={intensity} onChange={e => setIntensity(parseInt(e.target.value))} style={{ width:'100%'}}></input>
+                    </label>
                     <div>
                         Position: ( {x.toFixed(2)} | {y.toFixed(2)} )
                     </div>
-                    <div>
-                        When enabled, the mouse pointer will gravitate towards clickable elements during movement.
-                    </div>
+                    <p>
+                        When enabled, the mouse pointer will gravitate towards some clickable elements during movement.
+                    </p>
+                    <p>
+                        I created this experiment to test how an opinionated mouse pointer would feel on a new version of my personal website. You can find the current version, as well as other experiments, at <a href="https://goldst.dev">goldst.dev</a>.
+                    </p>
+                    <p>
+                        This project uses <code>@<a href="https://hatch.studio">hatch</a>/hds</code>, a propietary component library. The experiment itself is available under the MIT license.
+                    </p>
+                    <p>
+                        Fork this on GitHub | Impressum
+                    </p>
                 </HatchWrapper>
-                <HatchWrapper direction='row'>
-                    <HatchWrapper>
-                        <HatchButton variant='primary'>Test Button №1</HatchButton>
-                        <HatchButton>Test Button №2</HatchButton>
-                        <HatchButton variant='primary'>Test Button №3</HatchButton>
-                        <HatchButton>Test Button №4</HatchButton>
-                    </HatchWrapper>
-                    <HatchWrapper>
-                        <HatchButton>Test Button №5</HatchButton>
-                        <HatchButton variant='primary'>Test Button №6</HatchButton>
-                        <HatchButton>Test Button №7</HatchButton>
-                        <HatchButton variant='primary'>Test Button №8</HatchButton>
-                    </HatchWrapper>
-                    <HatchWrapper>
-                        <HatchButton variant='primary'>Test Button №9</HatchButton>
-                        <HatchButton>Test Button №10</HatchButton>
-                        <HatchButton variant='primary'>Test Button №11</HatchButton>
-                        <HatchButton>Test Button №12</HatchButton>
-                    </HatchWrapper>
-                    <HatchWrapper>
-                        <HatchButton>Test Button №13</HatchButton>
-                        <HatchButton variant='primary'>Test Button №14</HatchButton>
-                        <HatchButton>Test Button №15</HatchButton>
-                        <HatchButton variant='primary'>Test Button №16</HatchButton>
-                    </HatchWrapper>
-                </HatchWrapper>
-                <HatchWrapper style={{ width: '500px' }} space='10px'>
+                <HatchWrapper style={{ width: '200px' }} space='10px'>
                     <HatchRadio
-                        helpText="Additional text №1."
-                        label="Test radio №1"
+                        label='Test radio №1'
+                        helpText='Additional text №1.'
+                        className=''
                     />
                     <HatchRadio
-                        helpText="Additional text №2."
-                        label="Test radio №2"
+                        label='Test radio №2'
+                        helpText='Additional text №2.'
+                        className=''
                     />
                     <HatchRadio
-                        helpText="Additional text №3."
-                        label="Test radio №3"
+                        label='Test radio №3'
+                        helpText='Additional text №3.'
+                        className=''
                     />
                     <HatchRadio
-                        helpText="Additional text №4."
-                        label="Test radio №4"
+                        label='Test radio №4'
+                        helpText='Additional text №4.'
+                        className=''
                     />
                     <HatchRadio
-                        helpText="Additional text №5."
-                        label="Test radio №5"
+                        disabled={true}
+                        label='Disabled radio №5'
+                        helpText='Additional text №5.'
+                        className=''
                     />
                     <HatchRadio
-                        helpText="Additional text №6."
-                        label="Test radio №6"
+                        label='Test radio №6'
+                        helpText='Additional text №6.'
+                        className=''
                     />
                     <HatchRadio
-                        helpText="Additional text №7."
-                        label="Test radio №7"
+                        label='Test radio №7'
+                        helpText='Additional text №7.'
+                        className=''
+                    />
+                    <HatchRadio
+                        label='Test radio №8'
+                        helpText='Additional text №8.'
+                        className=''
                     />
                 </HatchWrapper>
+                <div className='buttons'>
+                    <HatchButton variant='primary'>Test Button № 1</HatchButton>
+                    <HatchButton disabled={true}>Disabled Button № 2</HatchButton>
+                    <HatchButton variant='primary'>Test Button № 3</HatchButton>
+                    <HatchButton disabled={true}>Disabled Button № 4</HatchButton>
+                    <HatchButton disabled={true}>Disabled Button № 5</HatchButton>
+                    <HatchButton variant='primary'>Test Button № 6</HatchButton>
+                    <HatchButton disabled={true}>Disabled Button № 7</HatchButton>
+                    <HatchButton variant='primary'>Test Button № 8</HatchButton>
+                    <HatchButton variant='primary'>Test Button № 9</HatchButton>
+                    <HatchButton disabled={true}>Disabled Button № 10</HatchButton>
+                    <HatchButton variant='primary'>Test Button № 11</HatchButton>
+                    <HatchButton disabled={true}>Disabled Button № 12</HatchButton>
+                    <HatchButton disabled={true}>Disabled Button № 13</HatchButton>
+                    <HatchButton variant='primary'>Test Button № 14</HatchButton>
+                    <HatchButton disabled={true}>Disabled Button № 15</HatchButton>
+                    <HatchButton variant='primary'>Test Button № 16</HatchButton>
+                </div>
             </HatchWrapper>
             <div className={`pointer ${locked ? '' : 'pointer--hidden'}`} style={{
                 left: `${x.toFixed(2)}px`,
                 top: `${y.toFixed(2)}px`
             }}></div>
+            <div className='escape-message' style={{
+                opacity: locked && showLeave ? 1 : 0
+            }}>
+                Press <kbd key='escape'>Esc</kbd> to leave
+            </div>
         </HatchFixedWidth>
     )
 }
